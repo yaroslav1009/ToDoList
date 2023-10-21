3b3b739431eda2267af4018ae511279253fa38e3
Ярослав <yaroslav@MBP-Aroslav.lan>
2023-10-21 15:57:53 +0300

    first2.1

--- script.js
+++ script.js
@@ -1,23 +1,64 @@
 let TaskValue = document.querySelector("#task");
 let ButtonAdd = document.querySelector("#ButtonAdd");
-let TaskList = document.querySelectorAll(".todolist_ul_li");
+let TaskList = document.querySelector(".todolist_ul_li");
 let ButtonDelete = document.querySelector("#ButtonDelete");
-const paragraph = document.querySelector(".todolist_ul_li_p");
+let ToDoList = document.querySelector(".todolist_ul")
+let taskItems = [];
 
-ButtonAdd.addEventListener('click', function(){
-    if(TaskValue.value !== ""){
-        /*add text in p */
-        TaskList.forEach(function(item) {
-            item.style.display = "flex";
-        });
-        paragraph.textContent = TaskValue.value;
-        TaskValue.value = "";
+TaskValue.addEventListener('keydown', function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault(); // Щоб уникнути додавання нового рядка
+        addButtonClickHandler();
     }
 });
 
-ButtonDelete.addEventListener('click', function(){
-    TaskList.forEach(function(item) {
-        item.style.display = "none";
-    });
-    TaskValue.value = "";
+ButtonAdd.addEventListener('click', addButtonClickHandler);
+
+ButtonDelete.addEventListener("click", () => {
+    // Видаляємо всі елементи <li> зі списку завдань
+    for (let item of taskItems) {
+        ToDoList.removeChild(item);
+    }
+
+    // Очищаємо масив taskItems
+    taskItems = [];
 });
+
+function addButtonClickHandler() {
+    if (TaskValue.value !== "") {
+        const li = document.createElement("li");
+        const p = document.createElement("p")
+        const btnDelete = document.createElement("button");
+        btnDelete.className = "todolist_ul_li_delete";
+        btnDelete.innerText = "Delete";
+        li.className = "todolist_ul_li";
+        p.innerText = TaskValue.value;
+        ToDoList.appendChild(li);
+        li.appendChild(p)
+        li.appendChild(btnDelete);
+
+        // Змінна для відстеження стану перечеркнення тексту
+        let isCrossedOut = false;
+
+        // Додаємо обробник події "click" для перечеркнення / розічеркування тексту
+        li.addEventListener("click", () => {
+            if (isCrossedOut) {
+                p.style.textDecoration = "none"; // Зняти перечеркнення
+            } else {
+                p.style.textDecoration = "line-through"; // Перечеркнути текст
+            }
+            isCrossedOut = !isCrossedOut; // Змінюємо стан перечеркнення
+        });
+
+        // Додаємо кнопку "Delete" в елемент <li>
+        li.appendChild(btnDelete);
+        
+        btnDelete.addEventListener("click", () => {
+            // Видаляємо батьківський елемент <li> з <ul>
+            ToDoList.removeChild(li);
+        });
+
+        taskItems.push(li);
+        TaskValue.value = "";
+    }
+}
